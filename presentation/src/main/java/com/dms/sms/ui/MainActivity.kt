e7777872b0da012d7dff3fe8b9be5fdb30a35dfd
윤석준 <tjrwns8024@naver.com>
2020-08-25 00:45:46 +0900

    [UPDATE] minSDK version, legacy

--- presentation/src/main/java/com/dms/sms/ui/MainActivity.kt
+++ presentation/src/main/java/com/dms/sms/ui/MainActivity.kt
@@ -7,6 +7,6 @@ import com.dms.sms.R
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.fragment_main_outing_card)
     }
 }
