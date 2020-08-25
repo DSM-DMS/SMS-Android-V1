286f300d63a6a4d09b6411a1878299e66b02ca84
윤석준 <tjrwns8024@naver.com>
2020-08-25 14:35:21 +0900

    [UPDATE] naming 변경

---
+++ presentation/src/main/java/com/dms/sms/ui/outingcard/MainGoOutFragment.kt
@@ -0,0 +1,20 @@
+package com.dms.sms.ui.outingcard
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.dms.sms.R
+
+class MainGoOutFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main_go_out, container, false)
+    }
+
+}
