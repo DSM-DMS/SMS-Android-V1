a25bdc99c36cc2cca2e0ee8a1cc62d2d924d817f
윤석준 <tjrwns8024@naver.com>
2020-08-27 15:06:41 +0900

    [MODIFY] package naming 변경

--- presentation/src/main/java/com/dms/sms/ui/fragment/outing/OutingNoticeFragment.kt
+++ presentation/src/main/java/com/dms/sms/ui/fragment/outing/OutingNoticeFragment.kt
@@ -1,4 +1,4 @@
-package com.dms.sms.ui.fragment.outingcard
+package com.dms.sms.ui.fragment.outing
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,14 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import com.dms.sms.R
 
-class OutingHistoryFragment : Fragment() {
+class OutingNoticeFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_outing_history, container, false)
+        return inflater.inflate(R.layout.fragment_outing_notice, container, false)
     }
 
 }
