ac7b9ae243a48b741880870e4e7d22adf3c10460
yujaemin <youjmen15@gmail.com>
2020-12-13 17:10:45 +0900

    [ADD] <viewmodel/ LoginViewModel.kt > add Login ErrorHandle

--- features/src/main/java/com/dms/sms/login/LoginViewModel.kt
+++ features/src/main/java/com/dms/sms/login/LoginViewModel.kt
@@ -3,30 +3,34 @@ package com.dms.sms.login
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import com.dms.domain.base.Result
 import com.dms.domain.auth.usecase.LoginUseCase
 import com.dms.domain.auth.dto.LoginResponseData
 import com.dms.sms.login.model.LoginModel
 import com.dms.sms.login.model.toDomain
+import com.dms.sms.viewmodel.BaseViewModel
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.observers.DisposableSingleObserver
+import com.dms.domain.base.Error
 
-class LoginViewModel(private val loginUseCase: LoginUseCase) : ViewModel() {
+class LoginViewModel(private val loginUseCase: LoginUseCase) : BaseViewModel() {
 
     val idText = MutableLiveData<String>()
     val passwordText = MutableLiveData<String>()
     val loginSuccessEvent: LiveData<Boolean> get() = _loginSuccessEvent
+    val isAutoLoginChecked = MutableLiveData<Boolean>().apply {
+        value = false
+    }
 
 
     private val _loginSuccessEvent = MutableLiveData<Boolean>()
 
     private val _isAllLoginInfoFilled = MediatorLiveData<Boolean>().apply {
         addSource(idText) {
-            value = it.isNotBlank() && !it.isNullOrEmpty()
+            value = !it.isNullOrBlank() && !passwordText.value.isNullOrBlank()
         }
         addSource(passwordText) {
-            value = it.isNotBlank() && !it.isNullOrEmpty()
+            value = !it.isNullOrBlank() && !idText.value.isNullOrBlank()
         }
     }
 
@@ -41,10 +45,34 @@ class LoginViewModel(private val loginUseCase: LoginUseCase) : ViewModel() {
                 override fun onSuccess(result: Result<LoginResponseData>) {
                     when(result){
                         is Result.Success ->{
+                            createToastEvent.value = "로그인에 성공하셨습니다"
                             _loginSuccessEvent.value = true
-
+                            if(isAutoLoginChecked.value!!){
+                                saveLoginData()
+                            }
                         }
                         is Result.Failure->{
+                            when(result.reason){
+                                Error.Conflict ->
+                                    createToastEvent.value = "존재하지 않는 학생입니다."
+                                Error.InternalServer ->
+                                    createToastEvent.value = "서버 오류 발생"
+                                Error.Network ->
+                                    createToastEvent.value = "네트워크 오류 발생"
+                                Error.BadRequest ->
+                                    createToastEvent.value = "로그인 실패"
+                                Error.UnAuthorized ->
+                                    createToastEvent.value = "로그인 실패"
+                                Error.Forbidden ->
+                                    createToastEvent.value = "로그인 실패"
+                                Error.NotFound ->
+                                    createToastEvent.value = "로그인 실패"
+                                Error.Timeout ->
+                                    createToastEvent.value = "요청하는데 시간이 너무 오래 걸립니다."
+                                Error.Unknown ->
+                                    createToastEvent.value = "알 수 없는 오류 발생"
+
+                            }
 
                         }
                     }
@@ -52,13 +80,21 @@ class LoginViewModel(private val loginUseCase: LoginUseCase) : ViewModel() {
                 }
 
                 override fun onError(e: Throwable) {
-
+                    createToastEvent.value = "로그인 실패"
+                    e.printStackTrace()
                 }
 
             },
             AndroidSchedulers.mainThread()
         )
 
+
+    }
+    private fun saveLoginData(){
+
+    }
+
+    private fun setAutoLogin(){
 
     }
 
@@ -69,6 +105,7 @@ class LoginViewModel(private val loginUseCase: LoginUseCase) : ViewModel() {
     private fun loginFailed() {
 
     }
+
 
 
 }
