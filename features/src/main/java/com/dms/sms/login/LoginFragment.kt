4f30d7bd734afea74adaed44e4ca156f9fd7750e
yujaemin <youjmen15@gmail.com>
2020-12-13 17:09:42 +0900

    [ADD] <login/ LoginFragment.kt> add toast event

--- features/src/main/java/com/dms/sms/login/LoginFragment.kt
+++ features/src/main/java/com/dms/sms/login/LoginFragment.kt
@@ -8,6 +8,7 @@ import com.dms.sms.databinding.FragmentLoginBinding
 import com.dms.sms.base.BaseFragment
 import com.dms.sms.navigateFragment
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import splitties.toast.toast
 
 
 class LoginFragment : BaseFragment<FragmentLoginBinding>() {
@@ -21,6 +22,10 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>() {
     override fun observeEvents() {
         viewModel.loginSuccessEvent.observe(viewLifecycleOwner, {
             navigateFragment(R.id.action_loginFragment_to_MainFragment)
+
+        })
+        viewModel.createToastEvent.observe(viewLifecycleOwner, {
+            toast(it)
         })
 
 
