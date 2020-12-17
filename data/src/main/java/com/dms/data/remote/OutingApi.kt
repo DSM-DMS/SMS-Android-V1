20afc3046420dfd4fae027ad4a54d215f01576be
윤석준 <tjrwns8024@naver.com>
2020-12-17 09:46:54 +0900

    [MODIFY] <api/ OutingApi.kt> change entity name

--- data/src/main/java/com/dms/data/remote/OutingApi.kt
+++ data/src/main/java/com/dms/data/remote/OutingApi.kt
@@ -1,13 +1,12 @@
 package com.dms.data.remote
 
-import com.dms.data.dto.request.OutingRequest
-import com.dms.data.dto.response.LoginResponse
-import com.dms.data.dto.response.OutingResponse
+import com.dms.data.dto.request.OutingData
+import com.dms.data.dto.response.OutingResponseData
 import io.reactivex.Single
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface OutingApi {
     @POST("v1/outings")
-    fun createOuting(@Body outingRequest: OutingRequest) : Single<OutingResponse>
+    fun createOuting(@Body outingData: OutingData) : Single<OutingResponseData>
 }
