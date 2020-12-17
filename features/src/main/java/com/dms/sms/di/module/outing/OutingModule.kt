fd272bbb69ae5bed1e8728223f5187cfb6a4bb6b
윤석준 <tjrwns8024@naver.com>
2020-12-17 09:36:17 +0900

    [ADD] <di/ OutingModule.kt> add OutingUseCase, OutingService, OutingRepository, OutingDataSource di

--- features/src/main/java/com/dms/sms/di/module/outing/OutingModule.kt
+++ features/src/main/java/com/dms/sms/di/module/outing/OutingModule.kt
@@ -1,5 +1,12 @@
 package com.dms.sms.di.module.outing
 
+import com.dms.data.datasource.OutingDataSource
+import com.dms.data.datasource.OutingDataSourceImpl
+import com.dms.data.repository.OutingRepositoryImpl
+import com.dms.domain.outing.repository.OutingRepository
+import com.dms.domain.outing.service.OutingService
+import com.dms.domain.outing.service.OutingServiceImpl
+import com.dms.domain.outing.usecase.OutingUseCase
 import com.dms.sms.feature.outing.viewmodel.OutingApplyViewModel
 import com.dms.sms.feature.outing.viewmodel.OutingViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
@@ -8,5 +15,10 @@ import org.koin.dsl.module
 
 val outingModule: Module = module {
     viewModel { OutingViewModel() }
-    viewModel { OutingApplyViewModel() }
+    viewModel { OutingApplyViewModel(get()) }
+
+    factory { OutingUseCase(get(), get()) }
+    factory<OutingService> { OutingServiceImpl(get(), get()) }
+    factory<OutingRepository> { OutingRepositoryImpl(get()) }
+    factory<OutingDataSource> { OutingDataSourceImpl(get()) }
 }
