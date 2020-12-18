a796b17d0042700c835eb199648f0f8ba42754dc
윤석준 <tjrwns8024@naver.com>
2020-12-18 09:10:44 +0900

    [ADD] <datasource/ OutingDataSource.kt> uuid 저장, 가져오기, 외출내역 리스트 추가

--- data/src/main/java/com/dms/data/datasource/OutingDataSource.kt
+++ data/src/main/java/com/dms/data/datasource/OutingDataSource.kt
@@ -1,9 +1,16 @@
 package com.dms.data.datasource
 
 import com.dms.data.dto.request.OutingData
+import com.dms.data.dto.response.OutingListResponseData
 import com.dms.data.dto.response.OutingResponseData
 import io.reactivex.Single
 
 interface OutingDataSource {
-    fun createOuting(outingData: OutingData) : Single<OutingResponseData>
+    fun createOuting(outingData: OutingData): Single<OutingResponseData>
+
+    fun getOutingList(studentUUID: String): Single<ArrayList<OutingListResponseData>>
+
+    fun saveOutingUUID(uuid: String, content: String)
+
+    fun getOutingUUID(content: String): String
 }
--- data/src/main/java/com/dms/data/datasource/OutingDataSourceImpl.kt
+++ data/src/main/java/com/dms/data/datasource/OutingDataSourceImpl.kt
@@ -1,11 +1,26 @@
 package com.dms.data.datasource
 
 import com.dms.data.dto.request.OutingData
+import com.dms.data.dto.response.OutingListResponseData
 import com.dms.data.dto.response.OutingResponseData
+import com.dms.data.local.sharedpreference.SharedPreferencesStorage
 import com.dms.data.remote.OutingApi
 import io.reactivex.Single
 
-class OutingDataSourceImpl(private val outingApi: OutingApi) : OutingDataSource {
+class OutingDataSourceImpl(
+    private val outingApi: OutingApi,
+    private val pref: SharedPreferencesStorage
+) : OutingDataSource {
     override fun createOuting(outingData: OutingData): Single<OutingResponseData> =
         outingApi.createOuting(outingData)
+
+    override fun getOutingList(studentUUID: String): Single<ArrayList<OutingListResponseData>> =
+        outingApi.getOutingList(studentUUID)
+
+    override fun saveOutingUUID(uuid: String, content: String) =
+        pref.saveInfo(uuid, "outingUUID")
+
+    override fun getOutingUUID(content: String) =
+        pref.getInfo("outingUUID")
+
 }
