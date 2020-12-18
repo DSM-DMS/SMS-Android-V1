9f2b9b8a872fd47b044eba53ab06fff2b8548faf
윤석준 <tjrwns8024@naver.com>
2020-12-18 16:19:13 +0900

    [MODIFY] <local/ SharedPreferencesStorage.kt> add content

--- data/src/main/java/com/dms/data/local/sharedpreference/SharedPreferencesStorage.kt
+++ data/src/main/java/com/dms/data/local/sharedpreference/SharedPreferencesStorage.kt
@@ -3,13 +3,18 @@ package com.dms.data.local.sharedpreference
 import android.content.Context
 import android.content.Context.MODE_PRIVATE
 import android.content.SharedPreferences
+import android.util.Log
 
 class SharedPreferencesStorage(private val context: Context) {
-    private var pref : SharedPreferences? = null
+    private var pref: SharedPreferences? = null
 
-    fun getInfo(content: String) : String{
+    fun getInfo(content: String): String {
         if (pref == null) pref = context.getSharedPreferences(content, MODE_PRIVATE)
-        return  "Bearer " + pref?.getString("info", "")
+        return if (content == "sms") {
+            "Bearer " + pref?.getString("info","")
+        } else
+            pref?.getString("info", "").toString()
+
     }
 
     fun saveInfo(info: String, content: String) {
@@ -19,9 +24,9 @@ class SharedPreferencesStorage(private val context: Context) {
         editor.apply()
     }
 
-    fun clearToken(content: String){
-        if(pref == null) pref = context.getSharedPreferences(content, MODE_PRIVATE)
-        val editor:SharedPreferences.Editor = pref!!.edit()
+    fun clearToken(content: String) {
+        if (pref == null) pref = context.getSharedPreferences(content, MODE_PRIVATE)
+        val editor: SharedPreferences.Editor = pref!!.edit()
         editor.clear()
         editor.apply()
     }
