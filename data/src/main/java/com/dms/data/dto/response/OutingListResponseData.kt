cd8aaac0f4cfe022c88045fb7a619c482b92ae88
윤석준 <tjrwns8024@naver.com>
2020-12-23 14:02:39 +0900

    [CREATE] <outing/ OutingListResponseData.kt>

---
+++ data/src/main/java/com/dms/data/dto/response/OutingListResponseData.kt
@@ -0,0 +1,43 @@
+package com.dms.data.dto.response
+
+import com.dms.domain.outing.response.OutingList
+import com.dms.domain.outing.response.OutingListResponse
+import com.dms.domain.outing.response.OutingResponse
+import com.google.gson.annotations.SerializedName
+
+data class OutingListResponseData(
+    val outings: List<OutingListData>
+)
+
+data class OutingListData(
+    @SerializedName("outing_uuid")
+    val outingUUID: String,
+    @SerializedName("place")
+    val place: String,
+    @SerializedName("reason")
+    val reason: String,
+    @SerializedName("start_time")
+    val startTime: Int,
+    @SerializedName("end_time")
+    val endTime: Int,
+    @SerializedName("outing_situation")
+    val outingSituation: String,
+    @SerializedName("outing_status")
+    val outingStatus: String
+)
+
+fun OutingListResponseData.toDomainData(): OutingListResponse =
+    OutingListResponse(this.outings.map{
+        it.toDomainData()
+    })
+
+fun OutingListData.toDomainData(): OutingList =
+    OutingList(
+        this.outingUUID,
+        this.place,
+        this.reason,
+        this.startTime,
+        this.endTime,
+        this.outingSituation,
+        this.outingStatus
+    )
