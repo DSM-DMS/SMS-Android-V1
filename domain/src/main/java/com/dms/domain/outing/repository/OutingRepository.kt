6ba63eb48a65148651b5c712b4f770e9173b2da6
윤석준 <tjrwns8024@naver.com>
2020-12-23 14:13:58 +0900

    [ADD] <repository/ OutingRepository.kt> add methods

--- domain/src/main/java/com/dms/domain/outing/repository/OutingRepository.kt
+++ domain/src/main/java/com/dms/domain/outing/repository/OutingRepository.kt
@@ -1,13 +1,18 @@
 package com.dms.domain.outing.repository
 
-import com.dms.domain.outing.request.OutingRequest
+import com.dms.domain.outing.request.OutingApplyRequest
+import com.dms.domain.outing.response.OutingListResponse
 import com.dms.domain.outing.response.OutingResponse
 import io.reactivex.Single
 
 interface OutingRepository {
-    fun createOuting(outingRequest: OutingRequest): Single<OutingResponse>
+    fun createOuting(outingApplyRequest: OutingApplyRequest): Single<OutingResponse>
+
+    fun getOutingList(studentUUID: String): Single<OutingListResponse>
 
     fun saveOutingUUID(uuid: String, content: String)
 
     fun getOutingUUID(content: String): String
+
+    fun getStudentUUID(content: String): String
 }
