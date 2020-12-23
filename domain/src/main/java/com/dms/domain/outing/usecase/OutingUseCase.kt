126bca08d1110a4592b3fc89aaaaa9185df6d24c
윤석준 <tjrwns8024@naver.com>
2020-12-23 14:16:22 +0900

    [MODIFY] <usecase/ OutingUseCase.kt> change method name

--- domain/src/main/java/com/dms/domain/outing/usecase/OutingUseCase.kt
+++ domain/src/main/java/com/dms/domain/outing/usecase/OutingUseCase.kt
@@ -2,14 +2,13 @@ package com.dms.domain.outing.usecase
 
 import com.dms.domain.base.Result
 import com.dms.domain.base.UseCase
-import com.dms.domain.outing.request.OutingRequest
-import com.dms.domain.outing.response.OutingResponse
+import com.dms.domain.outing.request.OutingApplyRequest
 import com.dms.domain.outing.service.OutingService
 import io.reactivex.Single
 import io.reactivex.disposables.CompositeDisposable
 
 class OutingUseCase(private val outingService: OutingService, disposable: CompositeDisposable) :
-    UseCase<OutingRequest, Result<Unit>>(disposable) {
-    override fun buildUseCase(data: OutingRequest): Single<Result<Unit>> =
+    UseCase<OutingApplyRequest, Result<Unit>>(disposable) {
+    override fun buildUseCase(data: OutingApplyRequest): Single<Result<Unit>> =
         outingService.createOuting(data)
 }
