216954af4bb3f4d85cc2f653c7982db0344869d9
윤석준 <tjrwns8024@naver.com>
2020-12-23 14:17:36 +0900

    [MODIFY] <outing/ OutingModule.kt> add OutingHistoryViewModel , GetOutingListUseCase Instance

--- features/src/main/java/com/dms/sms/di/module/outing/OutingModule.kt
+++ features/src/main/java/com/dms/sms/di/module/outing/OutingModule.kt
@@ -6,8 +6,10 @@ import com.dms.data.repository.OutingRepositoryImpl
 import com.dms.domain.outing.repository.OutingRepository
 import com.dms.domain.outing.service.OutingService
 import com.dms.domain.outing.service.OutingServiceImpl
+import com.dms.domain.outing.usecase.GetOutingListUseCase
 import com.dms.domain.outing.usecase.OutingUseCase
 import com.dms.sms.feature.outing.viewmodel.OutingApplyViewModel
+import com.dms.sms.feature.outing.viewmodel.OutingHistoryViewModel
 import com.dms.sms.feature.outing.viewmodel.OutingViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.module.Module
@@ -16,8 +18,10 @@ import org.koin.dsl.module
 val outingModule: Module = module {
     viewModel { OutingViewModel() }
     viewModel { OutingApplyViewModel(get()) }
+    viewModel { OutingHistoryViewModel(get()) }
 
     factory { OutingUseCase(get(), get()) }
+    factory { GetOutingListUseCase(get(), get()) }
     factory<OutingService> { OutingServiceImpl(get(), get()) }
     factory<OutingRepository> { OutingRepositoryImpl(get()) }
     factory<OutingDataSource> { OutingDataSourceImpl(get(), get()) }
