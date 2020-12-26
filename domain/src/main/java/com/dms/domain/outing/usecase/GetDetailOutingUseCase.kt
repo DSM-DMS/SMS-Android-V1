22ead7ac3fb46a7c86267516758b2755256d4c44
윤석준 <tjrwns8024@naver.com>
2020-12-27 00:52:26 +0900

    delete log import

--- domain/src/main/java/com/dms/domain/outing/usecase/GetDetailOutingUseCase.kt
+++ domain/src/main/java/com/dms/domain/outing/usecase/GetDetailOutingUseCase.kt
@@ -1,6 +1,5 @@
 package com.dms.domain.outing.usecase
 
-import android.util.Log
 import com.dms.domain.base.Result
 import com.dms.domain.base.UseCase
 import com.dms.domain.outing.response.DetailOutingResponse
@@ -10,8 +9,6 @@ import io.reactivex.disposables.CompositeDisposable
 
 class GetDetailOutingUseCase(private val outingService: OutingService, disposable: CompositeDisposable) :
     UseCase<String, Result<DetailOutingResponse>>(disposable){
-    override fun buildUseCase(data: String): Single<Result<DetailOutingResponse>> {
-        Log.d("outingUUID UseCase",data)
-        return outingService.getDetailOuting(data)
-    }
+    override fun buildUseCase(data: String): Single<Result<DetailOutingResponse>> =
+        outingService.getDetailOuting(data)
 }
