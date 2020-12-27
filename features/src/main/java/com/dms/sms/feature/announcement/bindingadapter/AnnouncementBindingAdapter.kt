525bdff95aaabfe99f6047b7db1f1d204092ad95
윤석준 <tjrwns8024@naver.com>
2020-12-27 22:42:07 +0900

    .

--- features/src/main/java/com/dms/sms/feature/announcement/bindingadapter/AnnouncementBindingAdapter.kt
+++ features/src/main/java/com/dms/sms/feature/announcement/bindingadapter/AnnouncementBindingAdapter.kt
@@ -1,12 +1,8 @@
 package com.dms.sms.feature.announcement.bindingadapter
 
 import androidx.databinding.BindingAdapter
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.dms.sms.feature.announcement.adapter.AnnouncementPageAdapter
 import com.dms.sms.feature.announcement.adapter.AnnouncementsAdapter
 import com.dms.sms.feature.announcement.model.SimpleAnnouncementModel
 import com.dms.sms.feature.announcement.viewmodel.AnnouncementsViewModel
@@ -20,9 +16,9 @@ fun RecyclerView.bindAnnouncementItems(viewModel: AnnouncementsViewModel,announc
     }
 
 }
-
-fun RecyclerView.bindAnnouncementPages(announcementPages: List<Int>) {
-    adapter = AnnouncementPageAdapter(announcementPages)
-    layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL,false)
-
-}
+//
+//fun RecyclerView.bindAnnouncementPages(announcementPages: List<Int>) {
+//    adapter = AnnouncementPageAdapter(announcementPages)
+//    layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL,false)
+//
+//}
