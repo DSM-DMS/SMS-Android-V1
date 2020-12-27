17b449008f12254c63fd623d1d37d5f4c4f74c6c
yujaemin <youjmen15@gmail.com>
2020-12-27 21:57:07 +0900

    [ADD] <model> presentation layer 공지 모델

---
+++ features/src/main/java/com/dms/sms/feature/announcement/model/AnnouncementModel.kt
@@ -0,0 +1,10 @@
+package com.dms.sms.feature.announcement.model
+
+data class AnnouncementModel(
+    val date: Int, val title: String, val content: String,
+    val writerName: String,
+    val nextTitle: String,
+    val nextAnnouncementUUID: String,
+    val previousTitle: String,
+    val previousAnnouncementUUID: String,
+)
