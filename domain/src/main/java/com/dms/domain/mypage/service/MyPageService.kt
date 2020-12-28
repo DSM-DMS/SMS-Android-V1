680c3a5bbddb9d15b304c7a12c6d94783a404b67
윤석준 <tjrwns8024@naver.com>
2020-12-28 20:27:40 +0900

    [ADD] <mypage/ MyPageService.kt> add changePw

--- domain/src/main/java/com/dms/domain/mypage/service/MyPageService.kt
+++ domain/src/main/java/com/dms/domain/mypage/service/MyPageService.kt
@@ -1,11 +1,15 @@
 package com.dms.domain.mypage.service
 
 import com.dms.domain.base.Result
+import com.dms.domain.mypage.request.PasswordRequest
 import com.dms.domain.mypage.response.UserResponse
+import io.reactivex.Completable
 import io.reactivex.Single
 
 interface MyPageService {
     fun getUserProfile(studentUUID: String): Single<Result<UserResponse>>
 
     fun getStudentUUID(): Single<Result<String>>
+
+    fun changePw(pw: PasswordRequest): Single<Result<Unit>>
 }
