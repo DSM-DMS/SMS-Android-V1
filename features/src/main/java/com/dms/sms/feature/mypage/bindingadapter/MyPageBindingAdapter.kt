6a3e2356ae7fe962b1507994bbb9544c26725e76
윤석준 <tjrwns8024@naver.com>
2020-12-29 11:13:51 +0900

    [ADD] <mypage/ MyPageBindingAdapter.kt> add developerList, setDeveloperProfile

--- features/src/main/java/com/dms/sms/feature/mypage/bindingadapter/MyPageBindingAdapter.kt
+++ features/src/main/java/com/dms/sms/feature/mypage/bindingadapter/MyPageBindingAdapter.kt
@@ -2,6 +2,10 @@ package com.dms.sms.feature.mypage.bindingadapter
 
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import androidx.lifecycle.MutableLiveData
+import androidx.recyclerview.widget.RecyclerView
+import com.dms.sms.feature.mypage.adapter.DeveloperAdapter
+import com.dms.sms.feature.mypage.model.DeveloperModel
 import com.dms.sms.util.GlideApp
 
 @BindingAdapter("setProfile")
@@ -9,6 +13,20 @@ fun ImageView.setProfile(profileUri: String?) {
     if (profileUri != null) {
         GlideApp.with(context)
             .load("https://dsm-sms-s3.s3.ap-northeast-2.amazonaws.com/$profileUri")
+            .into(this)
+    }
+}
+
+@BindingAdapter("developerList")
+fun RecyclerView.bindDeveloperList(developerItems: MutableLiveData<ArrayList<DeveloperModel>>) {
+    (adapter as DeveloperAdapter).setItems(developerItems)
+}
+
+@BindingAdapter("setDeveloperProfile")
+fun ImageView.setDeveloperProfile(path: Int?){
+    if(path != null){
+        GlideApp.with(context)
+            .load(path)
             .into(this)
     }
 }
