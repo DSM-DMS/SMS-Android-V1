a43dd1d28aac0f89ae024f0a8fd9353878e2f094
윤석준 <tjrwns8024@naver.com>
2020-12-30 16:09:04 +0900

    [ADD] <outing/ OutingRepository.kt>  add getPlaceList

--- domain/src/main/java/com/dms/domain/outing/repository/OutingRepository.kt
+++ domain/src/main/java/com/dms/domain/outing/repository/OutingRepository.kt
@@ -4,6 +4,7 @@ import com.dms.domain.outing.request.OutingApplyRequest
 import com.dms.domain.outing.response.DetailOutingResponse
 import com.dms.domain.outing.response.OutingListResponse
 import com.dms.domain.outing.response.OutingResponse
+import com.dms.domain.outing.response.SearchPlaceListResponse
 import io.reactivex.Single
 
 interface OutingRepository {
@@ -12,6 +13,8 @@ interface OutingRepository {
     fun getOutingList(studentUUID: String): Single<OutingListResponse>
 
     fun getDetailOuting(outingUUID: String): Single<DetailOutingResponse>
+
+    fun getPlaceList(keyword: String): Single<SearchPlaceListResponse>
 
     fun saveOutingUUID(uuid: String, content: String)
 
