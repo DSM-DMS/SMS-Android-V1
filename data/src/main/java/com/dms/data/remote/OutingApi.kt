f7d7de35e78e169584126b3a4d4ad0a767cd9b83
윤석준 <tjrwns8024@naver.com>
2020-12-30 15:04:36 +0900

    [ADD] <outing/ OutingApi.kt> getPlaceList

--- data/src/main/java/com/dms/data/remote/OutingApi.kt
+++ data/src/main/java/com/dms/data/remote/OutingApi.kt
@@ -4,11 +4,9 @@ import com.dms.data.dto.request.OutingData
 import com.dms.data.dto.response.DetailOutingResponseData
 import com.dms.data.dto.response.OutingListResponseData
 import com.dms.data.dto.response.OutingResponseData
+import com.dms.data.dto.response.SearchPlaceListResponseData
 import io.reactivex.Single
-import retrofit2.http.Body
-import retrofit2.http.GET
-import retrofit2.http.POST
-import retrofit2.http.Path
+import retrofit2.http.*
 
 interface OutingApi {
     @POST("v1/outings")
@@ -19,4 +17,7 @@ interface OutingApi {
 
     @GET("/v1/outings/uuid/{outing_uuid}/card")
     fun getDetailOuting(@Path("outing_uuid") outingUUID: String): Single<DetailOutingResponseData>
+
+    @GET("/naver-open-api/search/local")
+    fun getPlaceList(@Query("keyword") keyword: String): Single<SearchPlaceListResponseData>
 }
