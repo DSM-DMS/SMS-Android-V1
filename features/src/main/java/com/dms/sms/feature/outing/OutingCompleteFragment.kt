4e0f5cbc271a57311b1961b9e5360170c410bb9e
윤석준 <tjrwns8024@naver.com>
2020-12-31 09:04:52 +0900

    [MODIFY] <outing/ OutingCompleteFragment.kt> setText Color

--- features/src/main/java/com/dms/sms/feature/outing/OutingCompleteFragment.kt
+++ features/src/main/java/com/dms/sms/feature/outing/OutingCompleteFragment.kt
@@ -1,10 +1,11 @@
 package com.dms.sms.feature.outing
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.Html
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.dms.sms.R
 import com.dms.sms.navigateFragment
 import kotlinx.android.synthetic.main.fragment_outing_complete.*
@@ -24,6 +25,7 @@ class OutingCompleteFragment : Fragment() {
         outing_complete_btn.setOnClickListener {
             navigateFragment(R.id.action_outingCompleteFragment_to_mainFragment)
         }
+        outing_apply_complete_tv.text = Html.fromHtml(resources.getString(R.string.outing_apply_complete_tv))
     }
 
 }
