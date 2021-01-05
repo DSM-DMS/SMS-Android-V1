1d1b471b660bc1940841ff8374b8339fcf890653
윤석준 <tjrwns8024@naver.com>
2021-01-05 09:25:04 +0900

    [ADD] <outing/ OutingService.kt>

--- domain/src/main/java/com/dms/domain/outing/service/OutingServiceImpl.kt
+++ domain/src/main/java/com/dms/domain/outing/service/OutingServiceImpl.kt
@@ -2,12 +2,14 @@ package com.dms.domain.outing.service
 
 import com.dms.domain.base.Result
 import com.dms.domain.base.toResult
+import com.dms.domain.base.toSingleResult
 import com.dms.domain.errorhandler.ErrorHandler
 import com.dms.domain.outing.repository.OutingRepository
 import com.dms.domain.outing.request.OutingApplyRequest
 import com.dms.domain.outing.response.DetailOutingResponse
 import com.dms.domain.outing.response.OutingListResponse
 import com.dms.domain.outing.response.SearchPlaceListResponse
+import com.dms.domain.util.getDate
 import io.reactivex.Single
 
 class OutingServiceImpl(
@@ -16,8 +18,12 @@ class OutingServiceImpl(
 ) : OutingService {
     override fun createOuting(outingApplyRequest: OutingApplyRequest): Single<Result<Unit>> =
         outingRepository.createOuting(outingApplyRequest).map {
-            outingRepository.saveOutingUUID(it.outingUUID, "outingUUID")
+            outingRepository.saveOutingUUID(
+                it.outingUUID,
+                getDate(outingApplyRequest.startTime.toLong() * 1000)
+            )
         }.toResult(errorHandler)
+
 
     override fun getOutingList(studentUUID: String): Single<Result<OutingListResponse>> =
         outingRepository.getOutingList(studentUUID).toResult(errorHandler)
@@ -27,6 +33,9 @@ class OutingServiceImpl(
 
     override fun getPlaceList(keyword: String): Single<Result<SearchPlaceListResponse>> =
         outingRepository.getPlaceList(keyword).toResult(errorHandler)
+
+    override fun postOutingAction(action: String): Single<Result<Unit>> =
+        outingRepository.postOutingAction(action).toSingleResult(errorHandler)
 
     override fun getStudentUUID(): Single<Result<String>> =
         outingRepository.getStudentUUID().toResult(errorHandler)
