c04b3770d3c1535bc95946281a77ff71f2382cfd
윤석준 <tjrwns8024@naver.com>
2021-01-16 19:43:04 +0900

    [MODIFY] <outing/ PostOutingActionUseCase.kt>

--- domain/src/main/java/com/dms/domain/outing/usecase/PostOutingActionUseCase.kt
+++ domain/src/main/java/com/dms/domain/outing/usecase/PostOutingActionUseCase.kt
@@ -2,12 +2,13 @@ package com.dms.domain.outing.usecase
 
 import com.dms.domain.base.Result
 import com.dms.domain.base.UseCase
+import com.dms.domain.outing.request.AccessOutingRequest
 import com.dms.domain.outing.service.OutingService
 import io.reactivex.Single
 import io.reactivex.disposables.CompositeDisposable
 
 class PostOutingActionUseCase(private val outingService: OutingService, disposable: CompositeDisposable) :
-    UseCase<String, Result<Unit>>(disposable){
-    override fun buildUseCase(data: String): Single<Result<Unit>> =
+    UseCase<AccessOutingRequest, Result<Unit>>(disposable){
+    override fun buildUseCase(data: AccessOutingRequest): Single<Result<Unit>> =
         outingService.postOutingAction(data)
 }
