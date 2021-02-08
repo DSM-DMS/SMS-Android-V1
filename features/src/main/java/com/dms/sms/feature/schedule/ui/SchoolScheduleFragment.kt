c49491c9f7c5753a68071a10b847e9bebc3e5fa7
윤석준 <tjrwns8024@naver.com>
2021-02-08 21:44:50 +0900

    [MODIFY] <fragment/ SchoolScheduleFragment.kt> 주석처리

--- features/src/main/java/com/dms/sms/feature/schedule/ui/SchoolScheduleFragment.kt
+++ features/src/main/java/com/dms/sms/feature/schedule/ui/SchoolScheduleFragment.kt
@@ -10,10 +10,10 @@ import com.dms.sms.base.BaseFragment
 import com.dms.sms.databinding.FragmentSchoolScheduleBinding
 import com.dms.sms.feature.schedule.adapter.DetailScheduleAdapter
 import com.dms.sms.feature.schedule.adapter.SchoolCalendarAdapter
-import com.dms.sms.feature.schedule.viewmodel.SchoolScheduleViewModel
 import com.dms.sms.feature.schedule.generateMonth
 import com.dms.sms.feature.schedule.getCurrentDate
 import com.dms.sms.feature.schedule.model.ScheduleDateModel
+import com.dms.sms.feature.schedule.viewmodel.SchoolScheduleViewModel
 import com.dms.sms.navigateFragment
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -35,12 +35,12 @@ class SchoolScheduleFragment : BaseFragment<FragmentSchoolScheduleBinding>() {
             }
 
         })
-        viewModel.isSelected.observe(viewLifecycleOwner, {
-            if(binding.schoolCalendarRv.adapter!=null){
-                (binding.schoolCalendarRv.adapter as SchoolCalendarAdapter).notifyDataSetChanged() // 이 부분 주석 처리하면 클릭시 배경이 안 변함, 주석처리 하지않으면 클릭시 효과는 나오지만, item 위치가 변함
-            }
-
-        })
+//        viewModel.isSelected.observe(viewLifecycleOwner, {
+//            if(binding.schoolCalendarRv.adapter!=null){
+//                (binding.schoolCalendarRv.adapter as SchoolCalendarAdapter).notifyDataSetChanged() // 이 부분 주석 처리하면 클릭시 배경이 안 변함, 주석처리 하지않으면 클릭시 효과는 나오지만, item 위치가 변함
+//            }
+//
+//        })
         viewModel.selectedDateSchedule.observe(viewLifecycleOwner, {
             it?.let {
                 binding.detailSchoolScheduleRv.adapter = DetailScheduleAdapter(it,viewModel.currentMonth.value!!)
