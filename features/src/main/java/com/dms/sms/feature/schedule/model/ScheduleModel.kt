c3ff24561710d6a8298c3da661e4cb973ace5981
yujaemin <youjmen15@gmail.com>
2021-02-24 06:57:58 +0900

    [ADD] <schedule> 상세일정 달/일 출력 형식 변경

--- features/src/main/java/com/dms/sms/feature/schedule/model/ScheduleModel.kt
+++ features/src/main/java/com/dms/sms/feature/schedule/model/ScheduleModel.kt
@@ -2,7 +2,7 @@ package com.dms.sms.feature.schedule.model
 
 import com.dms.domain.schedule.entity.Schedule
 
-data class ScheduleModel(val scheduleUUID : String, val startDate : Int, val endDate : Int, val detail : String, var datePosition : Int = -1)
+data class ScheduleModel(val scheduleUUID : String, val startMonth : Int, val startDay : Int, val endMonth : Int, val endDay : Int, val detail: String, var datePosition : Int = -1)
 
 fun Schedule.toModel() : ScheduleModel =
-    ScheduleModel(this.scheduleUUID, this.startDate, this.endDate, this.detail)
+    ScheduleModel(this.scheduleUUID, this.startMonth, this.startDay, this.endMonth, this.endDay, this.detail)
