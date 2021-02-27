3fb53b1a89f04f978d019fa9db259443ada547ed
yujaemin <youjmen15@gmail.com>
2021-02-27 22:21:53 +0900

    [ADD] <di> 회원가입 클래스들 의존성 주입

---
+++ features/src/main/java/com/dms/sms/di/module/signup/SignUpModule.kt
@@ -0,0 +1,33 @@
+package com.dms.sms.di.module.signup
+
+import com.dms.data.datasource.SignUpDataSource
+import com.dms.data.datasource.SignUpDataSourceImpl
+import com.dms.data.repository.SignUpRepositoryImpl
+import com.dms.domain.signup.repository.SignUpRepository
+import com.dms.domain.signup.service.SignUpService
+import com.dms.domain.signup.service.SignUpServiceImpl
+import com.dms.domain.signup.usecase.GetNoAccountStudentInfoUseCase
+import com.dms.domain.signup.usecase.SignUpUseCase
+import com.dms.sms.di.module.provideSignUpApi
+import com.dms.sms.feature.schedule.viewmodel.SchoolScheduleViewModel
+import com.dms.sms.feature.signup.viewmodel.SignUpViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+
+val signUpModule = module {
+    single { SignUpViewModel(get(),get()) }
+
+    factory { SignUpUseCase(get(), get()) }
+    factory { GetNoAccountStudentInfoUseCase(get(), get()) }
+
+
+    factory<SignUpService>{ SignUpServiceImpl(get(),get()) }
+
+    factory<SignUpRepository> { SignUpRepositoryImpl(get()) }
+
+    factory<SignUpDataSource> { SignUpDataSourceImpl(get()) }
+
+    single { provideSignUpApi(get()) }
+
+}
