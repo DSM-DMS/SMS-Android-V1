5ab9c50494d3df2ee1207cf5a6f8055e0a1177f5
윤석준 <tjrwns8024@naver.com>
2021-03-01 23:45:44 +0900

    외출 안내 다이얼로그 생성

---
+++ features/src/main/java/com/dms/sms/feature/outing/dialog/OutingApplyNoticeDialog.kt
@@ -0,0 +1,18 @@
+package com.dms.sms.feature.outing.dialog
+
+import com.dms.sms.R
+import com.dms.sms.base.BaseDialog
+import com.dms.sms.databinding.FragmentOutingApplyNoticeDialogBinding
+import com.dms.sms.feature.outing.viewmodel.OutingApplyViewModel
+import org.koin.androidx.viewmodel.ext.android.sharedViewModel
+
+class OutingApplyNoticeDialog : BaseDialog<FragmentOutingApplyNoticeDialogBinding>() {
+    override val viewModel by sharedViewModel<OutingApplyViewModel>()
+
+    override val layoutId: Int
+        get() = R.layout.fragment_outing_apply_notice_dialog
+
+    override fun observeEvent() {
+
+    }
+}
