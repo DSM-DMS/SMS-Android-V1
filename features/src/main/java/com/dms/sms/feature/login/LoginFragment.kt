e8128438d98d378c5683ce79cdabebf73290ba7e
윤석준 <tjrwns8024@naver.com>
2021-03-02 11:25:15 +0900

    Endpointfragment Login

--- features/src/main/java/com/dms/sms/feature/login/LoginFragment.kt
+++ features/src/main/java/com/dms/sms/feature/login/LoginFragment.kt
@@ -1,18 +1,17 @@
 package com.dms.sms.feature.login
 
 import com.dms.sms.R
-import com.dms.sms.base.BaseFragment
+import com.dms.sms.base.EndPointBaseFragment
 import com.dms.sms.databinding.FragmentLoginBinding
 import com.dms.sms.navigateFragment
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import splitties.toast.toast
 
 
-class LoginFragment : BaseFragment<FragmentLoginBinding>() {
+class LoginFragment : EndPointBaseFragment<FragmentLoginBinding>() {
 
     override val layoutId: Int
         get() = R.layout.fragment_login
-
 
     override val viewModel: LoginViewModel by viewModel()
 
