a2f11d96c4d0fa18f203fa389599034bc2ff82a7
윤석준 <tjrwns8024@naver.com>
2021-03-03 10:21:01 +0900

    TimeTableService

--- domain/src/main/java/com/dms/domain/timetable/service/TimeTableService.kt
+++ domain/src/main/java/com/dms/domain/timetable/service/TimeTableService.kt
@@ -2,9 +2,9 @@ package com.dms.domain.timetable.service
 
 import com.dms.domain.base.Result
 import com.dms.domain.timetable.request.TimeTableDate
-import com.dms.domain.timetable.response.TimeTableResponse
+import com.dms.domain.timetable.response.TimeTableListResponse
 import io.reactivex.Single
 
 interface TimeTableService {
-    fun getTimeTable(timeTableDate: TimeTableDate): Single<Result<TimeTableResponse>>
+    fun getTimeTable(timeTableDate: TimeTableDate): Single<Result<TimeTableListResponse>>
 }
