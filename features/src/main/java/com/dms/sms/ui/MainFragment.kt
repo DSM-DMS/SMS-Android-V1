5fd5e49dfec36608d4f5ef73a3c33d39228278f1
yujaemin <youjmen15@gmail.com>
2021-03-15 01:09:36 +0900

    [ADD] 바텀 네비게이션 뷰페이저 추가

--- features/src/main/java/com/dms/sms/ui/MainFragment.kt
+++ features/src/main/java/com/dms/sms/ui/MainFragment.kt
@@ -8,6 +8,7 @@ import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import androidx.navigation.ui.setupWithNavController
+import androidx.viewpager2.widget.ViewPager2
 import com.dms.sms.R
 import com.dms.sms.base.BaseFragment
 import com.dms.sms.base.BaseViewModel
@@ -26,8 +27,39 @@ class MainFragment : BaseFragment<FragmentMainBinding>() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel.checkAnnouncementsUnread()
-        binding.mainNavigation.setupWithNavController(Navigation.findNavController(requireActivity(), R.id.main_container))
+        binding.mainVp.adapter= MainPagerAdapter(requireActivity().supportFragmentManager, lifecycle)
+        binding.mainVp.registerOnPageChangeCallback(PageChangeCallback())
         binding.mainNavigation.itemIconTintList =null
+        binding.mainNavigation.setOnNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.schedule_fragment ->{
+                    binding.mainVp.currentItem = 0
+                }
+                R.id.outing_fragment ->{
+                    binding.mainVp.currentItem = 1
+                }
+                R.id.notice_fragment ->{
+                    binding.mainVp.currentItem = 2
+                }
+                R.id.mypage_fragment ->{
+                    binding.mainVp.currentItem = 3
+                }
+
+            }
+            true
+        }
+    }
+    private inner class PageChangeCallback: ViewPager2.OnPageChangeCallback() {
+        override fun onPageSelected(position: Int) {
+            super.onPageSelected(position)
+            binding.mainNavigation.selectedItemId = when (position) {
+                0 -> R.id.schedule_fragment
+                1 -> R.id.outing_fragment
+                2 -> R.id.notice_fragment
+                3 -> R.id.mypage_fragment
+                else -> error("no such position: $position")
+            }
+        }
     }
 
 
---
+++ features/src/main/java/com/dms/sms/ui/MainPagerAdapter.kt
@@ -0,0 +1,26 @@
+package com.dms.sms.ui
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.dms.sms.feature.announcement.ui.AnnouncementsFragment
+import com.dms.sms.feature.mypage.MyPageFragment
+import com.dms.sms.feature.outing.OutingMainFragment
+import com.dms.sms.feature.schedule.ui.SchoolScheduleFragment
+
+class MainPagerAdapter(fm : FragmentManager, lc : Lifecycle) : FragmentStateAdapter(fm, lc){
+
+
+    override fun getItemCount(): Int =4
+
+    override fun createFragment(position: Int): Fragment {
+        return when (position) {
+            0 -> SchoolScheduleFragment()
+            1 -> OutingMainFragment()
+            2 -> AnnouncementsFragment()
+            else -> MyPageFragment()
+        }
+    }
+}
