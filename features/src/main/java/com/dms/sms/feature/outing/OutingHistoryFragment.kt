b51a5d85517cb4a2a8a4b4a675a9bae7cb6d6303
윤석준 <tjrwns8024@naver.com>
2021-03-17 10:44:37 +0900

    로그 삭제

--- features/src/main/java/com/dms/sms/feature/outing/OutingHistoryFragment.kt
+++ features/src/main/java/com/dms/sms/feature/outing/OutingHistoryFragment.kt
@@ -1,7 +1,6 @@
 package com.dms.sms.feature.outing
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.view.animation.AnimationUtils
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -34,7 +33,6 @@ class OutingHistoryFragment : BaseFragment<FragmentOutingHistoryBinding>() {
         })
 
         viewModel.clickOutingApplyEvent.observe(viewLifecycleOwner, {
-            Log.e("click","clickcolci")
             navigateFragment(R.id.action_outingHistoryFragment_to_outingApplyFragment)
         })
     }
