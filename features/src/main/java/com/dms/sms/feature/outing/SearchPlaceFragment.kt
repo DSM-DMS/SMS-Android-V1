f54f4fb69418ef6c23e5c5b729f97502032936f0
윤석준 <tjrwns8024@naver.com>
2021-03-17 10:45:03 +0900

    키보드 검색 버튼 활성화

--- features/src/main/java/com/dms/sms/feature/outing/SearchPlaceFragment.kt
+++ features/src/main/java/com/dms/sms/feature/outing/SearchPlaceFragment.kt
@@ -2,6 +2,7 @@ package com.dms.sms.feature.outing
 
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.dms.sms.R
 import com.dms.sms.base.BaseDialog
@@ -22,16 +23,28 @@ class SearchPlaceFragment : BaseDialog<FragmentSearchPlaceBinding>() {
         binding.searchPlaceRecyclerView.adapter = SearchPlaceAdapter(viewModel)
         binding.searchPlaceRecyclerView.layoutManager = LinearLayoutManager(context)
 
+        initView()
+
     }
 
     override fun observeEvent() {
-        with(viewModel){
+        with(viewModel) {
             searchPlaceEt.value = null
             searchPlaceList.value = ArrayList(emptyList())
 
             searchPlaceItemEvent.observe(viewLifecycleOwner, {
                 dismiss()
             })
+        }
+    }
+
+    private fun initView() {
+        binding.searchPlaceEt.setOnEditorActionListener { v, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                viewModel.onSearchPressed(binding.searchPlaceEt.text.trim().toString())
+                return@setOnEditorActionListener true
+            }
+            return@setOnEditorActionListener false
         }
     }
 
